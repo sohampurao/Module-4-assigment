fbf3530dd2f95ba94ca0dfa03a64a287adada5ab
Soham Purao <94833514+Soham-Purao-12@users.noreply.github.com>
2022-04-30 22:59:59 +0530

    Update javas.js

--- javas.js
+++ javas.js
@@ -1,14 +1,14 @@
 var names=new Array();
-names[0]="virat";
+names[0]="Yaakov";
 names[1]="John";
-names[2]="Jonny";
-names[3]="jackline";
-names[4]="rahul";
-names[5]="marrie";
-names[6]="laura";
-names[7]="chessy";
-names[8]="timmy";
-names[9]="jessy";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
 
 
 for (var i = 0; i < names.length; i++) {
